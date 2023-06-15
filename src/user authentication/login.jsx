f3ecea9a378c207e578f97c333af3d591e598181
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2023-06-15 01:21:10 +0000

    login and signup authentication

---
+++ src/user authentication/login.jsx
@@ -0,0 +1,73 @@
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { Link } from "react-router-dom"
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth, provider } from "../firebase";
+import "./style.scss"
+const Login = () => {
+    // const {}
+    let email, password
+    const handleSubmit = e => {
+        e.preventDefault()
+        email = e.target[0].value
+        password = e.target[1].value
+        
+        try{
+
+            signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            console.log(user)
+            // ...
+            })
+            .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorMessage)
+            });
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+
+    const handleClick = () => {
+        console.log('clicked')
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            const user = result.user;
+            console.log(user)
+        }).catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorMessage)
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+        });
+    }
+    return(
+        <div className="form-wrapper">
+            <form action="" onSubmit={handleSubmit}>
+                <h2>Sign In</h2>
+
+                <input type="email" placeholder="Email"/>
+                <input type="password" placeholder="Password"/>
+
+                <button type='submit'>Login</button>
+            </form>
+
+            <button id="google" onClick={handleClick}>
+            <i className="fa-brands fa-google"></i>
+                Sign In with Google
+            </button>
+            <p>Don't have an account?<Link to='/register'>Register</Link></p>
+        </div>
+    )
+}
+
+export default Login
---
+++ src/user authentication/register.jsx
@@ -0,0 +1,78 @@
+import { Link, useNavigate } from "react-router-dom"
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, provider, db, storage } from "../firebase";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { setDoc, doc, addDoc, collection } from "firebase/firestore";
+import "./style.scss"
+
+const Register = () => {
+    const navigate = useNavigate()
+
+    const handleSubmit = async e => {
+        e.preventDefault()
+        const displayName = e.target[0].value
+        const email = e.target[1].value
+        const password = e.target[2].value
+        
+        try{
+            const res = await createUserWithEmailAndPassword(auth, email, password)
+
+            await setDoc(doc(db, 'users', res.user.uid), {
+                uid: res.user.uid,
+                displayName,
+                email,
+            })
+
+            await setDoc(doc(db, 'files', res.user.uid), {})
+            navigate('/')
+        }
+        catch(err){
+            console.log(err)
+        }
+        
+    }
+    
+    const handleClick = async () => {
+        
+        signInWithPopup(auth, provider)
+        .then( async (result) => {
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            const user = result.user;
+            await setDoc(doc(db, 'users', user.uid), {
+                uid: user.uid,
+                displayName: user.displayName,
+                email: user.email
+            })
+            await setDoc(doc(db, 'files', user.uid), {})
+            navigate('/')
+        }).catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorMessage)
+            const email = error.customData.email;
+            const credential = GoogleAuthProvider.credentialFromError(error);
+        });
+    }
+
+    return(
+        <div className="form-wrapper">
+            <form action="" onSubmit={handleSubmit}>
+                <h2>Get Started</h2>
+
+                <input type="text" placeholder="Username"/>
+                <input type="email" placeholder="Email"/>
+                <input type="password" placeholder="Password"/>
+
+                <button type='submit'>Register</button>
+            </form>
+            <button id="google" onClick={handleClick}>
+            <i className="fa-brands fa-google"></i>
+                Sign up with Google
+            </button>
+            <p>Already have an account?<Link to='/'>Login</Link></p>
+        </div>
+    )
+}
+
+export default Register
