3f8ce27143adaffda812e9bb91cc4a7181781db7
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2023-06-24 01:08:33 +0000

    hot fix

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={window.location.pathname}>
+    <BrowserRouter>
       <ContextProvider>
         <App />
       </ContextProvider>
